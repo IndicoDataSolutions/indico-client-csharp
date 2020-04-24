cda8157d66f2acc667c393b29428a754883d29ac
Don Zereski <don@indico.io>
2020-04-24 11:59:16 -0400

    Fixed ModelTrainingProgress

--- GetModelTrainingProgress/Program.cs
+++ GetModelTrainingProgress/Program.cs
@@ -17,19 +17,18 @@ namespace Examples
                 Environment.Exit(0);
             }
 
-            int model_group_id = Int32.Parse(args[0]);
+            int mgId = Int32.Parse(args[0]);
 
             IndicoConfig config = new IndicoConfig(
                 host: "app.indico.io"             
             );
 
             IndicoClient client = new IndicoClient(config);
-            ModelGroupQuery modelGroupQuery = client.ModelGroupQuery();
-            TrainingModelWithProgressQuery trainingModelWithProgress = client.TrainingModelWithProgressQuery();
-            ModelGroup modelGroup = modelGroupQuery.Id(model_group_id).Query();
-            JObject trainingStatus = trainingModelWithProgress.Id(model_group_id).Query();
-            Console.WriteLine(modelGroup.Name);
-            Console.WriteLine(trainingStatus);
+            ModelGroup mg = client.ModelGroupQuery(mgId).Exec();
+            JArray trainingStatus = client.TrainingModelWithProgressQuery(mg).Exec();
+            
+            Console.WriteLine(mg.Name);
+            Console.WriteLine(trainingStatus);                     
         }
     }
 }
