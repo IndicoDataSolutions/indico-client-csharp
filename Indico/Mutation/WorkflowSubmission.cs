45606335eab47be5d2ee44aa2075a1c19327f4fa
Aditya Sadawarte <adityasadawarte01@gmail.com>
2020-10-30 15:20:54 +0530

    updated workflows

--- Indico/Mutation/WorkflowSubmission.cs
+++ Indico/Mutation/WorkflowSubmission.cs
@@ -1,101 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
-using GraphQL.Common.Request;
-using GraphQL.Common.Response;
-using Indico.Exception;
-using Indico.Jobs;
-using Indico.Storage;
-using Newtonsoft.Json.Linq;
 
 namespace Indico.Mutation
 {
-    public class WorkflowSubmission : Mutation<Job>
+    public class WorkflowSubmission : WorkflowSubmissionBase, Mutation<List<int>>
     {
-        IndicoClient _client;
         /// <summary>
         /// Workflow Id
         /// </summary>
         /// <value>Workflow Id</value>
-        public int Id { get; set; }
+        public override int WorkflowId { get; set; }
         /// <summary>
         /// Files to submit
         /// </summary>
         /// <value>Files</value>
-        public List<string> Files { get; set; }
+        public override List<string> Files { get; set; }
+        public override List<Stream> Streams { get; set; }
+        public override List<string> Urls { get; set; }
+        protected override bool Detailed { get; set; } = false;
 
-        public WorkflowSubmission(IndicoClient client)
+        public WorkflowSubmission(IndicoClient client) : base(client) { }
+
+        public async Task<List<int>> Exec()
         {
-            this._client = client;
-        }
+            JObject response = await base.Exec();
+            JArray ids = (JArray)response.GetValue("submissionIds");
 
-        /// <summary>
-        /// Executes request and returns Job
-        /// </summary>
-        /// <returns>Job</returns>
-        async public Task<Job> Exec()
-        {
-            JArray fileMetadata;
-            List<object> files = new List<object>();
-            fileMetadata = await this.Upload(this.Files);
-            foreach (JObject uploadMeta in fileMetadata)
-            {
-                JObject meta = new JObject
-                {
-                    { "name", (string)uploadMeta.GetValue("name") },
-                    { "path", (string)uploadMeta.GetValue("path") },
-                    { "upload_type", (string)uploadMeta.GetValue("upload_type") }
-                };
+            List<int> submissionIds = new List<int>();
+            foreach (JToken submissionId in ids)
+                submissionIds.Add((int)submissionId);
 
-                var file = new
-                {
-                    filename = (string)uploadMeta.GetValue("name"),
-                    filemeta = meta.ToString()
-                };
-
-                files.Add(file);
-            }
-
-            string query = @"
-                    mutation WorkflowSubmission($workflowId: Int!, $files: [FileInput]!) {
-                        workflowSubmission(workflowId: $workflowId, files: $files) {
-                            jobId
-                        }
-                    }
-                ";
-
-            GraphQLRequest request = new GraphQLRequest()
-            {
-                Query = query,
-                OperationName = "WorkflowSubmission",
-                Variables = new
-                {
-                    workflowId = this.Id,
-                    files
-                }
-            };
-
-            GraphQLResponse response = await this._client.GraphQLHttpClient.SendMutationAsync(request);
-            if (response.Errors != null)
-            {
-                throw new GraphQLException(response.Errors);
-            }
-
-            string jobId = (string)response.Data.workflowSubmission.jobId;
-            if (jobId == null)
-            {
-                throw new RuntimeException($"Failed to submit to workflow {this.Id}");
-            }
-
-            return new Job(this._client.GraphQLHttpClient, jobId);
-        }
-
-        async Task<JArray> Upload(List<string> filePaths)
-        {
-            UploadFile uploadRequest = new UploadFile(this._client)
-            {
-                Files = filePaths
-            };
-            return await uploadRequest.Call();
+            return submissionIds;
         }
     }
 }
---
+++ Indico/Query/GetWorkflow.cs
@@ -0,0 +1,35 @@
+﻿using Indico.Entity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Indico.Query
+{
+    public class GetWorkflow : Query<Workflow>
+    {
+        IndicoClient _client;
+        public int WorkflowId { get; set; }
+
+        public GetWorkflow(IndicoClient client) => this._client = client;
+
+        public async Task<Workflow> Exec()
+        {
+            ListWorkflows listWorkflows = new ListWorkflows(this._client)
+            {
+                WorkflowIds = new List<int> { this.WorkflowId }
+            };
+
+            List<Workflow> workflows = await listWorkflows.Exec();
+            if (workflows.Count != 0)
+            {
+                return workflows[0];
+            }
+            return null;
+        }
+
+        public Task<Workflow> Refresh(Workflow obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
