7a9ad76201dc34ab0c8d8d8a2e01db567170ce38
Aditya Sadawarte <adityasadawarte01@gmail.com>
2020-10-30 20:12:42 +0530

    Fixed SubmissionFilter issues

--- Indico/Entity/SubmissionFilter.cs
+++ Indico/Entity/SubmissionFilter.cs
@@ -1,26 +1,26 @@
 ﻿using Indico.Types;
 using System.Collections.Generic;
+using System.Dynamic;
 
 namespace Indico.Entity
 {
     public class SubmissionFilter : Filter
     {
         public string InputFilename { get; set; }
-        public SubmissionStatus Status { get; set; }
-        public bool Retrieved { get; set; }
+        public SubmissionStatus? Status { get; set; }
+        public bool? Retrieved { get; set; }
         public List<SubmissionFilter> OR { get; set; }
         public List<SubmissionFilter> AND { get; set; }
 
         public override dynamic ToAnonymousType()
         {
-            return new
-            {
-                inputFilename = InputFilename,
-                status = Status.ToString(),
-                retrieved = Retrieved,
-                ors = MergeFilters(OR),
-                ands = MergeFilters(AND)
-            };
+            dynamic anonymousType = new ExpandoObject();
+            if (InputFilename != null) anonymousType.inputFilename = InputFilename;
+            if (Status != null) anonymousType.status = Status.ToString();
+            if (Retrieved != null) anonymousType.retrieved = Retrieved;
+            if (OR != null) anonymousType.ors = MergeFilters(OR);
+            if (AND != null) anonymousType.ands = MergeFilters(AND);
+            return anonymousType;
         }
     }
 }
