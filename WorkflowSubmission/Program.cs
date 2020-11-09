550e2fa464436fce442a425fa6eae7bcf85f1b7b
Aditya Sadawarte <adityasadawarte01@gmail.com>
2020-11-09 12:57:45 +0530

    updated workflow submission example

--- WorkflowSubmission/Program.cs
+++ WorkflowSubmission/Program.cs
@@ -3,7 +3,7 @@ using Indico.Entity;
 using Indico.Jobs;
 using Indico.Mutation;
 using Indico.Query;
-using System;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -19,24 +19,41 @@ namespace Examples
 
             IndicoClient client = new IndicoClient(config);
 
-            // List Workflows for Dataset
+            // List Workflows for Dataset 1707
             ListWorkflows listWorkflows = new ListWorkflows(client)
             {
-                DatasetIds = new List<int>() { DATASET_ID }
+                DatasetIds = new List<int>() { 1707 }
             };
             List<Workflow> workflows = await listWorkflows.Exec();
 
             // Select Workflow
             Workflow workflow = workflows[0];
 
-            // Submit files to Workflow
             WorkflowSubmission workflowSubmission = new WorkflowSubmission(client)
             {
                 WorkflowId = workflow.Id,
-                Files = FILE_LIST
+                // Submit files to Workflow
+                Files = new List<string>() { "path-to-file" },
+                // Or submit streams to Workflow
+                Streams = // Stream List
             };
 
             List<int> submissionIds = await workflowSubmission.Exec();
+
+            // Select Submission
+            int submissionId = submissionIds[0];
+
+            SubmissionResult submissionResult = new SubmissionResult(client)
+            {
+                SubmissionId = submissionId
+            };
+
+            Job job = await submissionResult.Exec();
+
+            // Get Submission result
+            JObject result = await job.Result();
+            // Or results
+            JArray results = await job.Results();
         }
     }
 }
