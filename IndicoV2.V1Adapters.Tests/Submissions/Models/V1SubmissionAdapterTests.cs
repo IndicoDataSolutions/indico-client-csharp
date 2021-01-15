e05319ea2861c15b7ed8a16e8465b2ba3978e91d
Rafal Kaczka <rafalkaczka@venturedevs.com>
2021-01-15 16:04:28 +0100

    add V1SubmissionAdapter tests

---
+++ IndicoV2.V1Adapters.Tests/Submissions/Models/V1SubmissionAdapterTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Indico.Entity;
+using IndicoV2.V1Adapters.Submissions.Models;
+using NUnit.Framework;
+using V1Status = Indico.Types.SubmissionStatus;
+using V2Status = IndicoV2.Abstractions.Submissions.Models.SubmissionStatus;
+
+namespace IndicoV2.V1Adapters.Tests.Submissions.Models
+{
+    public class V1SubmissionAdapterTests
+    {
+        private static object[][] Status_ShouldCorrectlyMapV1Status_TestCaseSources = Enum.GetValues(typeof(V1Status))
+            .OfType<V1Status>()
+            .Select(s => new object[] {s, Enum.Parse<V2Status>(s.ToString())})
+            .ToArray();
+
+        [TestCaseSource(nameof(Status_ShouldCorrectlyMapV1Status_TestCaseSources))]
+        public void Status_ShouldCorrectlyMapV1Status(V1Status v1Status, V2Status expectedV2Status)
+        {
+            // Arrange
+            var sut = new V1SubmissionAdapter(new Submission{ Status = v1Status });
+
+            // Act
+            var v2Status = sut.Status;
+
+            // Assert
+            v2Status.Should().Be(expectedV2Status);
+        }
+    }
+}
