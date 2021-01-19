fd3eb6f92621e3eebe240248f553d81e6f392a44
Rafal Kaczka <rafalkaczka@venturedevs.com>
2021-01-19 11:40:35 +0100

    INT-35 add DotFx

---
+++ docfx.cmd
@@ -0,0 +1,1 @@
+%USERPROFILE%\.nuget\packages\docfx.console\2.56.6\tools\docfx.exe %1 %2 %3
---
+++ docfx_project/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace DocFx
+{
+    public class Program
+    {
+        public static void Main(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(c =>
+                c
+                    .UseWebRoot("_site/")
+                    .UseStartup<DocFxStartup>()
+                )
+            .Start();
+    }
+
+    public class DocFxStartup
+    {
+        public static void Configure(IApplicationBuilder app) => app
+            .UseDefaultFiles()
+            .UseStaticFiles()
+        ;
+    }
+}
