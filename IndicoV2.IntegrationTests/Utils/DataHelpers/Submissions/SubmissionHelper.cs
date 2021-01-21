9dbdc6e7c499467b7f51f3e95848c1ecbed74817
Szymon Socha <szymon.socha@venturedevs.com>
2021-01-21 21:48:48 +0100

    INT-33 Added submission list async integration test.

--- IndicoV2.IntegrationTests/Utils/DataHelpers/Submissions/SubmissionHelper.cs
+++ IndicoV2.IntegrationTests/Utils/DataHelpers/Submissions/SubmissionHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+
 using IndicoV2.DataSets;
 using IndicoV2.Submissions;
 using IndicoV2.Submissions.Models;
@@ -19,10 +20,21 @@ namespace IndicoV2.IntegrationTests.Utils.DataHelpers.Submissions
             var dataSets = await _dataSets.ListAsync();
             var workflows = await _workflows.ListAsync(dataSets.First().Id);
             // TODO: can be replaced with fetching first of existing
-            var submissionIds = await _submissions.CreateAsync(workflows.First().Id, new [] { content ?? new MemoryStream()});
+            var submissionIds = await _submissions.CreateAsync(workflows.First().Id, new[] { content ?? new MemoryStream() });
             var submission = await _submissions.GetAsync(submissionIds.Single());
 
             return submission;
+        }
+
+        public async Task<(int workflowId, int submissionId)> ListAnyAsync(Stream content = null)
+        {
+            var dataSets = await _dataSets.ListAsync();
+            int dataSetId = dataSets.First().Id;
+            var workflows = await _workflows.ListAsync(dataSetId);
+            int workflowId = workflows.First().Id;
+            var submissionIds = await _submissions.CreateAsync(workflowId, new[] { content ?? new MemoryStream() });
+
+            return (workflowId, submissionIds.First());
         }
 
         public SubmissionHelper(IDataSetClient dataSets, IWorkflowsClient workflows, ISubmissionsClient submissions)
