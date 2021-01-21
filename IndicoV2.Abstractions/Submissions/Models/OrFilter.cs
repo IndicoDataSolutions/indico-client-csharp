7811f7e0064f89ece6456d7fc329dacb04e1bea9
Szymon Socha <szymon.socha@venturedevs.com>
2021-01-21 13:16:56 +0100

    INT-33 Added new submission filters structure.

---
+++ IndicoV2.Abstractions/Submissions/Models/OrFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace IndicoV2.Submissions.Models
+{
+    public class OrFilter : MergeableFilter
+    {
+        public List<IFilter> Or { get; set; }
+
+        public override dynamic ToAnonymousType()
+        {
+            dynamic anonymousType = new ExpandoObject();
+            
+            if (Or != null)
+                anonymousType.ors = MergeFilters(Or);
+
+            return anonymousType;
+        }
+    }
+}
---
+++ IndicoV2.Abstractions/Submissions/Models/SubmissionFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace IndicoV2.Submissions.Models
+{
+    public class SubmissionFilter : IFilter
+    {
+        public string InputFilename { get; set; }
+        public SubmissionStatus? Status { get; set; }
+        public bool? Retrieved { get; set; }
+
+        public dynamic ToAnonymousType()
+        {
+            dynamic anonymousType = new ExpandoObject();
+
+            if (InputFilename != null)
+                anonymousType.inputFilename = InputFilename;
+            if (Status != null)
+                anonymousType.status = Status.ToString();
+            if (Retrieved != null)
+                anonymousType.retrieved = Retrieved;
+
+            return anonymousType;
+        }
+    }
+}
