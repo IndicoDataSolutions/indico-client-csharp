9dae529338106c93d21c5565d41fad65621907b3
Szymon Socha <szymon.socha@venturedevs.com>
2021-01-21 18:02:34 +0100

    Changed submission status converter.

--- IndicoV2.V1Adapters/Converters/SubmissionStatusConverters.cs
+++ IndicoV2.V1Adapters/Converters/SubmissionStatusConverters.cs
@@ -8,42 +8,24 @@ namespace IndicoV2.V1Adapters.Converters
     {
         public static Indico.Types.SubmissionStatus? ConvertToLegacy(this SubmissionStatus? status)
         {
-            switch (status)
-            {
-                case SubmissionStatus.PROCESSING:
-                    return Indico.Types.SubmissionStatus.PROCESSING;
-                case SubmissionStatus.FAILED:
-                    return Indico.Types.SubmissionStatus.FAILED;
-                case SubmissionStatus.COMPLETE:
-                    return Indico.Types.SubmissionStatus.COMPLETE;
-                case SubmissionStatus.PENDING_ADMIN_REVIEW:
-                    return Indico.Types.SubmissionStatus.PENDING_ADMIN_REVIEW;
-                case SubmissionStatus.PENDING_REVIEW:
-                    return Indico.Types.SubmissionStatus.PENDING_REVIEW;
+            if (!status.HasValue)
+                return null;
 
-                default:
-                    throw new NotImplementedException();
-            }
+            if (!Enum.IsDefined(typeof(Indico.Types.SubmissionStatus), status))
+                throw new NotImplementedException();
+
+            return (Indico.Types.SubmissionStatus)status;
         }
 
         public static SubmissionStatus? ConvertFromLegacy(this Indico.Types.SubmissionStatus? legacyStatus)
         {
-            switch (legacyStatus)
-            {
-                case Indico.Types.SubmissionStatus.PROCESSING:
-                    return SubmissionStatus.PROCESSING;
-                case Indico.Types.SubmissionStatus.FAILED:
-                    return SubmissionStatus.FAILED;
-                case Indico.Types.SubmissionStatus.COMPLETE:
-                    return SubmissionStatus.COMPLETE;
-                case Indico.Types.SubmissionStatus.PENDING_ADMIN_REVIEW:
-                    return SubmissionStatus.PENDING_ADMIN_REVIEW;
-                case Indico.Types.SubmissionStatus.PENDING_REVIEW:
-                    return SubmissionStatus.PENDING_REVIEW;
+            if (!legacyStatus.HasValue)
+                return null;
 
-                default:
-                    throw new NotImplementedException();
-            }
+            if (!Enum.IsDefined(typeof(SubmissionStatus), legacyStatus))
+                throw new NotImplementedException();
+
+            return (SubmissionStatus)legacyStatus;
         }
     }
 }
