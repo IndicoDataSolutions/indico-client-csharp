0312f72450701df78aab70502369b68e443d88c2
Szymon Socha <szymon.socha@venturedevs.com>
2021-02-02 17:13:52 +0100

    INT-37 Added integration tests for creating submission from URI.

--- IndicoV2.IntegrationTests/Utils/DataHelpers/DataHelper.cs
+++ IndicoV2.IntegrationTests/Utils/DataHelpers/DataHelper.cs
@@ -1,5 +1,6 @@
 ﻿using IndicoV2.IntegrationTests.Utils.DataHelpers.Files;
 using IndicoV2.IntegrationTests.Utils.DataHelpers.Submissions;
+using IndicoV2.IntegrationTests.Utils.DataHelpers.Uris;
 using IndicoV2.IntegrationTests.Utils.DataHelpers.Workflows;
 using Unity;
 
@@ -9,15 +10,15 @@ namespace IndicoV2.IntegrationTests.Utils.DataHelpers
     {
         private readonly IUnityContainer _container;
 
-        public DataHelper(IUnityContainer container)
-        {
-            _container = container;
-        }
-        
+        public DataHelper(IUnityContainer container) => _container = container;
+
         public SubmissionHelper Submissions() => _container.Resolve<SubmissionHelper>();
         
         public WorkflowHelper Workflows() => _container.Resolve<WorkflowHelper>();
 
         public FileHelper Files() => _container.Resolve<FileHelper>();
+
+        public UriHelper Uris() => _container.Resolve<UriHelper>();
+
     }
 }
---
+++ IndicoV2.IntegrationTests/Utils/DataHelpers/Uris/UriHelper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace IndicoV2.IntegrationTests.Utils.DataHelpers.Uris
+{
+    public class UriHelper
+    {
+        public Uri GetSampleUri() => new Uri("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf");
+    }
+}
