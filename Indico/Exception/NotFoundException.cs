6d3f4c69555b8c30008ce552c0e3622ee63a2d63
Szymon Socha <szymon.socha@venturedevs.com>
2021-03-29 13:27:26 +0200

    INT-100 Added NotFoundException.

---
+++ Indico/Exception/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Indico.Exception
+{
+    [Serializable]
+    public class NotFoundException : System.Exception
+    {
+        public NotFoundException() { }
+
+        public NotFoundException(string message) : base(message) { }
+    }
+}
