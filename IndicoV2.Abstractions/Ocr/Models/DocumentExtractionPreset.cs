8ab507d49d32d923abcc47875037aa6027f489e2
rkaczka-vd <76998421+rkaczka-vd@users.noreply.github.com>
2021-04-02 15:42:34 +0200

    INT-107 config enum (#64)
    * INT-107 add DocumentExtractionPreset enum
    
    * INT-107 fix local nuget build

---
+++ IndicoV2.Abstractions/Ocr/Models/DocumentExtractionPreset.cs
@@ -0,0 +1,30 @@
+﻿namespace IndicoV2.Ocr.Models
+{
+    public enum DocumentExtractionPreset
+    {
+        /// <summary>
+        /// Provides page text and block text/position in a nested format.
+        /// </summary>
+        Standard,
+
+        /// <summary>
+        ///  Provides a simple and fast response for native PDFs (3-5x faster). Will NOT work with scanned PDFs.
+        /// </summary>
+        Simple,
+
+        /// <summary>
+        /// Provided to mimic the behavior of Indico’s older pdf_extraction function. Use this if your model was trained with data from the older pdf_extraction.
+        /// </summary>
+        Legacy,
+
+        /// <summary>
+        /// Provides detailed bounding box information on tokens and characters. Returns data in a nested format at the document level with all metadata included.
+        /// </summary>
+        Detailed,
+
+        /// <summary>
+        /// Provides detailed information at the page-level in an unnested format.
+        /// </summary>
+        OnDocument,
+    }
+}
