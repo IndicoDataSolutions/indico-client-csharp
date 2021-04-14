5ef01766cc376ee849f0a61ee8a80eab1076cc75
Szymon Socha <szymon.socha@venturedevs.com>
2021-04-14 15:41:44 +0200

    INT-128 Added new exception for failed status when getting result.

---
+++ IndicoV2.Abstractions/Extensions/SubmissionResult/Exceptions/WrongSubmissionStatusException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IndicoV2.Extensions.SubmissionResult.Exceptions
+{
+    public class WrongSubmissionStatusException : Exception
+    {
+        public WrongSubmissionStatusException()
+        {
+        }
+
+        public WrongSubmissionStatusException(string message) : base(message)
+        {
+        }
+
+        public WrongSubmissionStatusException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
