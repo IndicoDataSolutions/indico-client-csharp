dfd19832ff30e03608289d3cce7c9d330f46b1ea
Szymon Socha <szymon.socha@venturedevs.com>
2021-04-15 08:50:49 +0200

    INT-128 Added WrongSubmissionStatusException constructor that works with SubmissionStatus.

--- IndicoV2.Abstractions/Extensions/SubmissionResult/Exceptions/WrongSubmissionStatusException.cs
+++ IndicoV2.Abstractions/Extensions/SubmissionResult/Exceptions/WrongSubmissionStatusException.cs
@@ -1,19 +1,17 @@
 ﻿using System;
+using IndicoV2.Submissions.Models;
 
 namespace IndicoV2.Extensions.SubmissionResult.Exceptions
 {
+    /// <summary>
+    /// Thrown when trying to get submission result and because of wrong status result cannot be downloaded.
+    /// </summary>
     public class WrongSubmissionStatusException : Exception
     {
-        public WrongSubmissionStatusException()
-        {
-        }
+        public WrongSubmissionStatusException() { }
 
-        public WrongSubmissionStatusException(string message) : base(message)
-        {
-        }
+        public WrongSubmissionStatusException(string message) : base(message) { }
 
-        public WrongSubmissionStatusException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
+        public WrongSubmissionStatusException(SubmissionStatus status) : base($"Cannot get the result because submission status is { status }.") { }
     }
 }
