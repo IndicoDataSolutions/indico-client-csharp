ad26cd634941266b5513b3c3b9129c8873c4bbe1
Rafal Kaczka <rafalkaczka@venturedevs.com>
2021-04-23 09:51:32 +0200

    add missing tests

--- IndicoV2.V1Adapters.Tests/Converters/CastingConverterTests.cs
+++ IndicoV2.V1Adapters.Tests/Converters/CastingConverterTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using IndicoV2.V1Adapters.Converters;
 using Newtonsoft.Json;
 using NUnit.Framework;
@@ -11,12 +12,24 @@ namespace IndicoV2.V1Adapters.Tests.Converters
         public void CastingConverter_ShouldUseImplicitCastOperator(int value) => 
             JsonConvert.DeserializeObject<TestClass>(value.ToString()).ValueProperty.Should().Be(value);
 
+        [Test]
+        public void CastingConverter_ShouldThrow_WHenNoCastOperator() =>
+            this.Invoking(_ => JsonConvert.DeserializeObject<TestNoCastOperator>("1"))
+                .Should().Throw<InvalidOperationException>()
+                .WithMessage("*operator not found*");
+
         [JsonConverter(typeof(CastingConverter<int>))]
         private class TestClass
         {
             public int ValueProperty { get; private set; }
 
-            public static implicit operator TestClass(int value) => new TestClass {ValueProperty = value};
+            public static implicit operator TestClass(int value) => new() {ValueProperty = value};
+        }
+
+        [JsonConverter(typeof(CastingConverter<int>))]
+        private class TestNoCastOperator
+        {
+            public int ValueProperty { get; private set; }
         }
     }
 }
