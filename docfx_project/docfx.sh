ef85536902dfcfb9511e76bd7eb818cdbd9f4424
Rafal Kaczka <rafalkaczka@venturedevs.com>
2021-06-16 10:33:59 +0200

    INT-153 publish documentation

--- docfx_project/docfx.sh
+++ docfx_project/docfx.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ $# -eq 0 ]]; then
+if [ $# -eq 0 ]; then
     PARAMS="--serve"
 else
     PARAMS="$1 $2 $3"
 fi
 
-DOCFX="$HOME/.nuget/packages/docfx.console/2.56.6/tools/docfx.exe DocFx.json $PARAMS"
+NUGET="./nuget"
+DOCFX="$NUGET/docfx.console/2.58.0/tools/docfx.exe DocFx.json $PARAMS"
 
-dotnet restore
+cd docfx_project
+dotnet restore --packages $NUGET
 $DOCFX
