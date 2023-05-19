e7b71ec332701f679769217cf5ee5f66be958027
Andrew Fitzgerald <andrew.fitzgerald@indico.io>
2023-05-19 13:27:24 -0400

    update logic for workflow id gathering and update README

--- IndicoV2.IntegrationTests/Workflows/WorkflowClientTests.cs
+++ IndicoV2.IntegrationTests/Workflows/WorkflowClientTests.cs
@@ -18,32 +18,45 @@ namespace IndicoV2.IntegrationTests.Workflows
         private IndicoConfigs _indicoConfigs;
         private IWorkflowsClient _workflowsClient;
         private int _workflowId;
+        private int _dataSetId;
 
         [SetUp]
         public async Task SetUp()
         {
             var container = new IndicoTestContainerBuilder().Build();
             _dataSetClient = container.Resolve<IDataSetClient>();
-            _workflowsClient = container.Resolve<IWorkflowsClient>();
-            _dataHelper = container.Resolve<DataHelper>();
-            _indicoConfigs = new IndicoConfigs();
-            int _rawWorkflowID = _indicoConfigs.WorkflowId;
-            if (_rawWorkflowID == 0)
+            var _rawDataSetId = _indicoConfigs.DatasetId;
+            if (_rawDataSetId == 0)
             {
-                var workflow = (await _dataHelper.Workflows().GetAnyWorkflow());
-                _workflowId = workflow.Id;
+                var dataset = (await _dataHelper.DataSets().GetAny());
+                _dataSetId = dataset.Id;
+                var workflows = await _workflowsClient.ListAsync(_dataSetId, default);
+                _workflowId = workflows.First().Id;
             }
             else
             {
-                _workflowId = _rawWorkflowID;
+                _dataSetId = _rawDataSetId;
+                int _rawWorkflowID = _indicoConfigs.WorkflowId;
+                if (_rawWorkflowID == 0)
+                {
+                    var workflows = await _workflowsClient.ListAsync(_dataSetId, default);
+                    _workflowId = workflows.First().Id;
+                }
+                else
+                {
+                    _workflowId = _rawWorkflowID;
+                }
             }
+            _workflowsClient = container.Resolve<IWorkflowsClient>();
+            _dataHelper = container.Resolve<DataHelper>();
+            _indicoConfigs = new IndicoConfigs();
         }
 
         [Test]
         public async Task AddData_ShouldReturnResult()
         {
-            var dataSetId = _indicoConfigs.DatasetId;
-            await _dataSetClient.AddFilesAsync(dataSetId, new[] {_dataHelper.Files().GetSampleFilePath()}, default);
+
+            await _dataSetClient.AddFilesAsync(_dataSetId, new[] {_dataHelper.Files().GetSampleFilePath()}, default);
             var result = await _workflowsClient.AddDataAsync(_workflowId, default);
 
             result.Should().NotBeNull();
