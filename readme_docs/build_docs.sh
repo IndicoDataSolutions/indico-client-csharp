caa2d5fb7acece5bc6c86cb6745cd20bd99a488d
meghanhickey <meghan.hickey@indico.io>
2023-11-08 13:46:26 -0500

    don't try to nest csharp docs

--- readme_docs/build_docs.sh
+++ readme_docs/build_docs.sh
@@ -2,23 +2,26 @@
 
 dotnet tool install docfx
 dotnet tool run docfx docfx_project/docfx.json 
-cd docfx_project
+# cd docfx_project
 
-for filename in apiV2/*.md; do
-    if [ "$(basename $filename)" != "IndicoV2.md" ]; then
-        newdir="$(dirname "$(echo $filename | sed -r 's|\.|\/|g; s|/([^/]*)$|.md|g')")" 
-        # create the parent directory
-        mkdir -p $newdir
-        # move file into correct directory
-        newfilename="$(basename $filename | sed -r 's|(.+)\.md|\1|; s|\.|-|g')"
-        newlocation="${newdir}/${newfilename}.md"
-        mv $filename $newlocation
-    fi
-done
+# for filename in apiV3/*.md; do
+#     if [ "$(basename $filename)" != "IndicoV2.md" ]; then
+#         newdir="$(dirname "$(echo $filename | sed -r 's|\.|\/|g; s|/([^/]*)$|.md|g')")" 
+#         # create the parent directory
+#         mkdir -p $newdir
+#         echo $newdir
+#         # move file into correct directory
+#         newfilename="$(basename $filename | sed -r 's|(.+)\.md|\1|; s|\.|-|g')"
+#         echo $newfilename
+#         newlocation="${newdir}/${newfilename}.md"
+#         echo $newlocation
+#         mv $filename $newlocation
+#     fi
+# done
 
-mv ../readme_docs/CommonModels.md ./apiV2/IndicoV2/IndicoV2-CommonModels.md
-mv ../readme_docs/Extensions.md ./apiV2/IndicoV2/IndicoV2-Extensions.md
+# mv ../readme_docs/CommonModels.md ./apiV2/IndicoV2/IndicoV2-CommonModels.md
+# mv ../readme_docs/Extensions.md ./apiV2/IndicoV2/IndicoV2-Extensions.md
 
-# fix links
-find ./apiV2 -type f -name '*.md' -print0 | xargs -0 sed -i -r 's|IHasCursor\\-1|IHasCursor-1|; s|(.+)\.md|\1|; s|\.|-|g'
-cd ..
+# # fix links
+# find ./apiV2 -type f -name '*.md' -print0 | xargs -0 sed -i -r 's|IHasCursor\\-1|IHasCursor-1|; s|(.+)\.md|\1|; s|\.|-|g'
+# cd ..
