2d4b4edf51344f6c5cdf6bd5b1beb594205f7972
meghanhickey <meghan.hickey@indico.io>
2023-11-07 19:23:04 -0500

    fix build script

--- readme_docs/build_docs.sh
+++ readme_docs/build_docs.sh
@@ -2,24 +2,23 @@
 
 dotnet tool install docfx
 dotnet tool run docfx docfx_project/docfx.json 
+cd docfx_project
 
-mv docfx_project/apiV2/* apiV2_docs
-
-for filename in apiV2_docs/*.md; do
+for filename in apiV2/*.md; do
     if [ "$(basename $filename)" != "IndicoV2.md" ]; then
         newdir="$(dirname "$(echo $filename | sed -r 's|\.|\/|g; s|/([^/]*)$|.md|g')")" 
         # create the parent directory
         mkdir -p $newdir
         # move file into correct directory
-        newfilename="$(basename $filename | sed -r 's|IndicoV2\.(.+)\.md|\1|; s|\.|-|g')"
+        newfilename="$(basename $filename | sed -r 's|(.+)\.md|\1|; s|\.|-|g')"
         newlocation="${newdir}/${newfilename}.md"
         mv $filename $newlocation
     fi
 done
 
-mv readme_docs/CommonModels.md apiV2_docs/IndicoV2/CommonModels.md
-mv readme_docs/Extensions.md apiV2_docs/IndicoV2/Extensions.md
+mv ../readme_docs/CommonModels.md ./apiV2/IndicoV2/IndicoV2-CommonModels.md
+mv ../readme_docs/Extensions.md ./apiV2/IndicoV2/IndicoV2-Extensions.md
 
-cd ./apiV2_docs
 # fix links
-find . -type f -name '*.md' -print0 | xargs -0 sed -i -r 's|IHasCursor\\-1|IHasCursor-1|; s|IndicoV2\.(.+)\.md|\1|; s|\.|-|g'
+find ./apiV2 -type f -name '*.md' -print0 | xargs -0 sed -i -r 's|IHasCursor\\-1|IHasCursor-1|; s|(.+)\.md|\1|; s|\.|-|g'
+cd ..
