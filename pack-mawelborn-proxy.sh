6252349097bad2c2a51dd7f9bad57388dada63d9
Michael Welborn <michael.welborn@indicodata.ai>
2024-05-07 20:46:00 -0500

    Update nupkg hash

--- pack-mawelborn-proxy.sh
+++ pack-mawelborn-proxy.sh
@@ -1,4 +1,4 @@
 podman build -f Dockerfile -t mawelborn-proxy
 podman create --name mawelborn-proxy-container mawelborn-proxy
-podman cp mawelborn-proxy-container:/indico-client-csharp/IndicoV2/bin/Release/IndicoClient.6.6.0-mawelborn-proxy-8fcdcb7dec51954781d5862424838dd5aad634bd.nupkg .
+podman cp mawelborn-proxy-container:/indico-client-csharp/IndicoV2/bin/Release/IndicoClient.6.6.0-mawelborn-proxy-e49f9dc863434d37f9f3dd366a4bb55050bb3e02.nupkg .
 podman rm mawelborn-proxy-container
